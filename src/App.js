ed078a0ff9ab567ea9b26a84484ee21325c5ff8a
santiago <felipesantiagolds@gmail.com>
2021-03-02 12:24:31 -0300

    Fecth Api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dogObj: 'Dogs',
+    };
+  }
+
+
+  componentDidMount() {
+    this.fecthApi()
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.dogObj.message.includes("terrier")) {
+      return false;
+    }
+    return true;
+  }
+
+  fecthApi = async () => {
+    const req = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await req.json();
+    this.setState({ dogObj: data });
+  };
+
+  render() {
+    if (this.state.dogObj === "") return "loading...";
+    return (
+      <div className="App">
+        <h1>Foto</h1>
+        <div>
+          <button onClick={this.fecthApi}>Novo cachorro!</button>
+          <div>
+            <img src={ this.state.dogObj.message } alt='dogs'/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
